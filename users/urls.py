01acd83d0ebc94272bc724027915419438a76114
Lucas Masteguim <lcsmstgm@gmail.com>
2026-04-26 23:27:13 -0300

    Login e cadastro funcionando com Django

---
+++ users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import cadastro_view
+
+urlpatterns = [
+    path("cadastro/", cadastro_view, name="cadastro"),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-# Create your views here.
+
+def cadastro_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+
+    for field in form.fields.values():
+        field.widget.attrs.update({"class": "form-control"})
+
+    return render(request, "users/cadastro.html", {"form": form})
