3981cd50f612382f6207c3e7890344204b1e4024
joojdesaas <Joaocbezerra26@gmail.com>
2026-05-12 17:46:19 -0300

    Criado o view do admininastro para o site

--- analytics/views.py
+++ analytics/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import TemplateView
+from django.contrib.auth import get_user_model
+from goals.models import Goal
 
-# Create your views here.
+User = get_user_model()
+
+class AdminDashboardView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
+    template_name = 'dashboard/home.html'
+
+    def test_func(self):
+        return self.request.user.is_staff
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_metas'] = Goal.objects.count()
+        context['usuarios_ativos'] = User.objects.filter(is_active=True).count()
+        return context
--- mdsis/urls.py
+++ mdsis/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
+
+# IMPORT CORRIGIDO: apontando para o app onde a view foi criada
+from analytics.views import AdminDashboardView
+
 from goals.views import (
     dashboard_view,
     metas_list_view,
@@ -14,62 +18,28 @@ from goals.views import (
 )
 
 urlpatterns = [
-
-    path('admin/', admin.site.urls),
-
+    # Rota raiz redireciona para o login
     path('', lambda request: redirect('login')),
 
-    path(
-        'dashboard/',
-        dashboard_view,
-        name='home'
-    ),
+    # Caminho específico para o seu Admin Customizado
+    path('painel/', AdminDashboardView.as_view(), name='admin_home'),
 
-    path(
-        'metas/',
-        metas_list_view,
-        name='metas_list'
-    ),
+    # Admin padrão do Django
+    path('admin/', admin.site.urls),
 
-    path(
-        'metas/nova/',
-        meta_create_view,
-        name='meta_create'
-    ),
+    # Demais rotas
+    path('dashboard/', dashboard_view, name='home'),
+    path('metas/', metas_list_view, name='metas_list'),
+    path('metas/nova/', meta_create_view, name='meta_create'),
+    path('indicadores/', indicadores_view, name='indicadores'),
 
-    path(
-        'indicadores/',
-        indicadores_view,
-        name='indicadores'
-    ),
+    path('perfil/', TemplateView.as_view(template_name='users/perfil.html'), name='perfil'),
 
-    path(
-        'perfil/',
-        TemplateView.as_view(
-            template_name='users/perfil.html'
-        ),
-        name='perfil'
-    ),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('users/', include('users.urls')),
 
-    path(
-        'accounts/',
-        include('django.contrib.auth.urls')
-    ),
-
-    path(
-        'users/',
-        include('users.urls')
-    ),
-    path(
-        'metas/<int:meta_id>/editar/', meta_edit_view, name='meta_edit'
-    ),
-    path(
-        'metas/<int:meta_id>/concluir/', meta_concluir_view, name='meta_concluir'
-    ),
-    path(
-        'metas/<int:meta_id>/incrementar/', meta_incrementar_view, name='meta_incrementar'
-         ),
-    path(
-        'metas/<int:meta_id>/excluir/', meta_delete_view, name='meta_delete'
-         ),
+    path('metas/<int:meta_id>/editar/', meta_edit_view, name='meta_edit'),
+    path('metas/<int:meta_id>/concluir/', meta_concluir_view, name='meta_concluir'),
+    path('metas/<int:meta_id>/incrementar/', meta_incrementar_view, name='meta_incrementar'),
+    path('metas/<int:meta_id>/excluir/', meta_delete_view, name='meta_delete'),
 ]
